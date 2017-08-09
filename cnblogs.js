e7cf361173907d051a8bea1d98f47cb3fe724a73
kklldog <kklldog@qq.com>
2017-08-10 02:04:07 +0800

    update

--- cnblogs.js
+++ cnblogs.js
@@ -5,7 +5,7 @@ var http = require('http');
 
 db.init();
 
-var successCallback = function (body) {
+var successCallback = function (body, mailAddress) {
     var $ = cheerio.load(body);
     $('div.post_item_body').each((index, postBody) => {
         var name = $(postBody).find('a.titlelnk').text();
@@ -16,25 +16,24 @@ var successCallback = function (body) {
             var eIndex = text.indexOf(')');
             var viewCount = text.substr(sIndex + 1, eIndex - sIndex - 1);
             var intViewCount = parseInt(viewCount);
-            console.log(link + ' ' + viewCount+' '+name);
+            console.log(link + ' ' + viewCount + ' ' + name);
             if (intViewCount > 1000) {
-               // console.log(link + ' ' + viewCount+' '+name);
-                trySendMail(link,name);
+                // console.log(link + ' ' + viewCount+' '+name);
+                trySendMail(link, name,mailAddress);
             }
         });
 
     });
-
-
 }
 
-var trySendMail = function (url,name) {
+var trySendMail = function (url, name,address) {
     db.find('cnblogsnotice', { url: url }, function (row) {
         if (row.length == 0) {
             //send mail
             var mail = {
                 body: url,
-                subject: '博客园推送 -' + name
+                subject: '博客园推送 -' + name,
+                address:address
             };
 
             var bodyHmtl = JSON.stringify(mail);
@@ -51,17 +50,17 @@ var trySendMail = function (url,name) {
     })
 }
 
-var filterPage = function (startIndex, endIndex, success) {
+var filterPage = function (startIndex, endIndex, success,address) {
     if (startIndex > endIndex) return;
 
     var url = 'https://www.cnblogs.com/mvc/AggSite/PostList.aspx';
     var postBody = { "CategoryType": "SiteHome", "ParentCategoryId": 0, "CategoryId": 808, "PageIndex": startIndex, "TotalPostCount": 4000, "ItemListActionName": "PostList" };
 
     myhttp.post(url, postBody, true, (res) => {
-        success(res.body);
+        success(res.body,address);
         if (startIndex <= endIndex) {
             var nextIndex = startIndex + 1;
-            filterPage(nextIndex, endIndex, success);
+            filterPage(nextIndex, endIndex, success,address);
         }
     }, (err) => {
         console.log(err);
@@ -69,6 +68,35 @@ var filterPage = function (startIndex, endIndex, success) {
 
 }
 
+var findMailAddress = function (callback) {
+    var mailAddress = [];
+    var fun = (i) => {
+        var url = "http://www.cnblogs.com/mvc/blog/GetComments.aspx?postId=7326178&blogApp=kklldog&anchorCommentId=0&pageIndex=";
+        myhttp.get(url + i, (res) => {
+            var body = res.body;
+            var bodyObj = JSON.parse(body);
+            var $ = cheerio.load(bodyObj.commentsHtml);
+            var comments = $('div.blog_comment_body');
+            if (comments.length > 0) {
+                comments.each((i, e) => {
+                    var comment = $(e).text();
+                    var MAIL_REGEXP = /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/;
+                    if(MAIL_REGEXP.test(comment)){
+                        mailAddress.push(comment);
+                        console.log(comment);
+                    }
+                });
+                fun(i + 1);
+            } else {
+                callback(mailAddress);
+            }
+        }, (err) => { }, 3);
+    };
+    fun(1);
+}
+
 exports.filter = function (start, end) {
-    filterPage(start, end, successCallback);
+    findMailAddress((address) => {
+        filterPage(start, end, successCallback,address);
+    });
 }
