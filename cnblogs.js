b264ef61b001df4f0317721d0700ee2a18846d21
kkllodg <kkllodg@qq.com>
2017-08-09 14:32:28 +0800

    UPDATE

--- cnblogs.js
+++ cnblogs.js
@@ -5,39 +5,44 @@ var http = require('http');
 
 db.init();
 
-var successCallback = function(body){
+var successCallback = function (body) {
     var $ = cheerio.load(body);
-    $('div.post_item_body .article_view a').each((i,e)=>{
-        var link = $(e).attr('href');
-        var text = $(e).text();
-        var sIndex = text.indexOf('(');
-        var eIndex = text.indexOf(')');
-        var viewCount = text.substr(sIndex+1,eIndex-sIndex-1);
-         
+    $('div.post_item_body').each((index, postBody) => {
+        var name = $(postBody).find('a.titlelnk').text();
+        $(postBody).find('span.article_view a').each((i, e) => {
+            var link = $(e).attr('href');
+            var text = $(e).text();
+            var sIndex = text.indexOf('(');
+            var eIndex = text.indexOf(')');
+            var viewCount = text.substr(sIndex + 1, eIndex - sIndex - 1);
+            var intViewCount = parseInt(viewCount);
+            console.log(link + ' ' + viewCount+' '+name);
+            if (intViewCount > 1000) {
+               // console.log(link + ' ' + viewCount+' '+name);
+                trySendMail(link,name);
+            }
+        });
 
-        var intViewCount = parseInt(viewCount);
-        if(intViewCount>1000){
-            console.log(link+' '+viewCount);   
-            trySendMail(link);
-        }
     });
+
+
 }
 
-var trySendMail = function(url){
-    db.find('cnblogsnotice',{url:url},function(row){
-        if(row.length == 0){
+var trySendMail = function (url,name) {
+    db.find('cnblogsnotice', { url: url }, function (row) {
+        if (row.length == 0) {
             //send mail
             var mail = {
-                body :url,
-                subject:'博客园推送'
+                body: url,
+                subject: '博客园推送 -' + name
             };
 
             var bodyHmtl = JSON.stringify(mail);
             console.log(bodyHmtl);
-            myhttp.post('http://localhost:3200/cnblogsnotice', bodyHmtl,false, (res) => {
+            myhttp.post('http://localhost:3200/cnblogsnotice', bodyHmtl, false, (res) => {
                 console.log(res.body);
 
-                db.insert('cnblogsnotice',{url:url});
+                db.insert('cnblogsnotice', { url: url });
 
             }, (err) => {
                 console.log(err);
@@ -46,24 +51,24 @@ var trySendMail = function(url){
     })
 }
 
-var filterPage = function(startIndex,endIndex,success){
-    if(startIndex>endIndex) return;
+var filterPage = function (startIndex, endIndex, success) {
+    if (startIndex > endIndex) return;
 
     var url = 'https://www.cnblogs.com/mvc/AggSite/PostList.aspx';
-    var postBody = {"CategoryType":"SiteHome","ParentCategoryId":0,"CategoryId":808,"PageIndex":startIndex,"TotalPostCount":4000,"ItemListActionName":"PostList"};
+    var postBody = { "CategoryType": "SiteHome", "ParentCategoryId": 0, "CategoryId": 808, "PageIndex": startIndex, "TotalPostCount": 4000, "ItemListActionName": "PostList" };
 
-    myhttp.post(url,postBody,true,(res)=>{
+    myhttp.post(url, postBody, true, (res) => {
         success(res.body);
-        if(startIndex<=endIndex){
-            var nextIndex = startIndex+1;   
-            filterPage(nextIndex,endIndex,success);
+        if (startIndex <= endIndex) {
+            var nextIndex = startIndex + 1;
+            filterPage(nextIndex, endIndex, success);
         }
-    },(err)=>{
+    }, (err) => {
         console.log(err);
-    },3);
+    }, 3);
 
 }
 
-exports.filter = function(start,end){
-    filterPage(start,end,successCallback);
+exports.filter = function (start, end) {
+    filterPage(start, end, successCallback);
 }
